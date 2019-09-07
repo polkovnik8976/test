3923d57bce85049f53030d8cb503c65e6368fcc7
Ärtem <ninjage@yandex.ru>
2019-09-07 22:47:18 +0300

    Это наш первый комит

---
+++ Obuczenie/script.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var leftBorderWidth = 1;
+let second = 2;
+const pi = 3.14;
+
+
+let person = {
+    name: "John",
+    age: "25",
+    isMarried: "false"
+}
+
+console.log(person["age"]);
+
+alert("Hello World!");
